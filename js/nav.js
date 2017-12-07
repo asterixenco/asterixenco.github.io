d15a864ec2a3e517f13cf69d8828f9af243e0db3
Pieter Mantel <4pietermantel@gmail.com>
2017-12-07 14:55:19 +0100

    nav #9

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,8 @@
 $("document").ready(function() {
 	var $navitem = $("#nav").contents().find(".navitem");
-	
+	for(int i = 0; i < $navitem; i++) {
+		$navitem.eq(i).click(function() {
+			alert(hi);
+		});
+	}
 });
