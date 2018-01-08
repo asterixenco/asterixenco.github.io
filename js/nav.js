ff1e5f2ad86b06791a4106f7c2206ce90adcb73d
Pieter Mantel <4pietermantel@gmail.com>
2018-01-08 13:04:54 +0100

    Nav #16

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-$("document").ready(function() {
+$(document).ready(function() {
 	var $navitem = $("#nav").contents().find(".navitem");
 	//alert($navitem);
 	$("#nav").contents().find(".navitem").on("click", function(){
