7caee59a792e22ec32dc1cc3a774cba31cccfc2a
Pieter Mantel <4pietermantel@gmail.com>
2018-01-12 10:52:58 +0100

    Nav #19

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ $(window).bind("load", function() {
 	var $navitem = $("#nav").contents().find(".navitem");
 	//alert($navitem);
 	$("#nav").contents().find(".navitem").on("click", function(){
-		alert("hi");
+		alert(this.attr("data-linkid"));
 	});
 });
 
